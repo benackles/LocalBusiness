cfa17e489b29ceaf005f5b5764c49d8a571eac29
benackles <benackles@gmail.com>
2021-08-03 17:51:05 -0700

    Add meta description to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ const IndexPage = () => {
       <Helmet>
           <meta charSet="utf-8" />
           <title>Local Business: Build Local Business Websites with Gatsby</title>
+          <meta name="description" content="Build your small business on a solid foundation of mobile-friendliness, performance, accessibility, SEO and structured data types."></meta>
           <link rel="canonical" href="" />
       </Helmet>
       <h1>Local Business</h1>
